0507c1968d6618f5fc8c40e246ae7c83ab4300da
kevin <iscarryon@gmail.com>
2017-07-17 22:58:16 +0800

    extract shape from layout string

--- create_image.py
+++ create_image.py
@@ -12,8 +12,8 @@ red = (255,0,0,125)
 blue = (0,0,255,125)
 
 dict_layer_color = {}
-dict_layer_color['OD'] = red
-dict_layer_color['POLY'] = blue
+dict_layer_color['6.0'] = red
+dict_layer_color['17.0'] = blue
 
 
 def y_from_top(y_from_bottom, img_height):
@@ -36,12 +36,26 @@ def draw_rectangle(draw_obj, layer_name, list_coor, img_size):
 	draw_obj.text(text_pos, text, fill=fill_color)
 	draw_obj.rectangle(list_coor, fill=fill_color, outline=None)
 
+def get_layer_and_coor_list(layout_string):
+	list_return = []
+
+	list_shape = layout_string.split(',')
+	for shape in list_shape:
+		layer = shape.split('@')[0]
+		coor_string = shape.split('@')[1]
+		coor_list = coor_string.split('_')
+		coor_list = list(map(int, coor_list))
+		list_this_shape = [layer, coor_list]
+		list_return.append(list_this_shape)
+
+	return list_return
 
 if __name__ == '__main__':
 	print('Start~')
 	
-	od_shape = [50,50,300,100]
-	poly_shape = [100,0,150,200]
+	layout_string = '6.0@50_50_300_100,17.0@100_0_150_200'
+
+	list_layout = get_layer_and_coor_list(layout_string)
 
 	# size of the image to create
 	size = (500,300)       
@@ -52,12 +66,16 @@ if __name__ == '__main__':
 	# create a drawing object that is RGBA
 	draw = ImageDraw.Draw(im, 'RGBA')   
 
-	draw_rectangle(draw, 'OD', od_shape, size)
-	draw_rectangle(draw, 'POLY', poly_shape, size)
-
+	for shape in list_layout:
+		layer = shape[0]
+		print(layer)
+		coor_list = shape[1]
+		print(coor_list)
+		draw_rectangle(draw, layer, coor_list, size)
+	
 	del draw
 
 	# write to stdout
-	im.save("bbb.png", "PNG")
+	im.save("ccc.png", "PNG")
 
 	print('Done~')
