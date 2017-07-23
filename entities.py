7933d5c07519317c2030bc68f01f5621d8239552
kevin <iscarryon@gmail.com>
2017-07-24 00:33:47 +0800

    update entities.py

--- entities.py
+++ entities.py
@@ -1,20 +1,29 @@
 # coding=UTF-8
 import sqlite3
 
+class Layout_Shape:
+	def __init__(self):
+		self.uid = -1
+		self.shape = ''
+		self.layout_name = ''
+		self.remark = ''
+
 class Layout_Env:
 	def __init__(self):
-		self.uid = ''
+		self.uid = -1
 		self.center_shape = ''
 		self.other_shape = ''
 		self.zone = ''
 		self.distance = 0.0
+		self.layout_name = ''
 		self.remark = ''
 
 class Layout_Net:
 	def __init__(self):
-		self.uid = ''
+		self.uid = -1
 		self.start_shape = ''
 		self.net_shape = ''
+		self.layout_name = ''
 		self.remark = ''
 
 def is_table_exist(pDatabase, pTable):
@@ -26,16 +35,21 @@ def is_table_exist(pDatabase, pTable):
 
 def prepare_table():
 	"create database tables"
-	database_name = 'stock.sqlite3'
+	database_name = 'layout.sqlite3'
 	conn = sqlite3.connect(database_name)
 	cursor = conn.cursor()
 
+	if is_table_exist(database_name, 'Layout_Shape') == None:
+	    cursor.execute('CREATE TABLE Layout_Shape (uid integer AUTOINCREMENT, shape text, layout_name text, remark text)')
+	    conn.commit()
+
+
 	if is_table_exist(database_name, 'Layout_Env') == None:
-	    cursor.execute('CREATE TABLE Layout_Env (uid text, center_shape text, other_shape text, zone text, distance real, remark text)')
+	    cursor.execute('CREATE TABLE Layout_Env (uid integer AUTOINCREMENT, center_shape text, other_shape text, zone text, distance real, layout_name text, remark text)')
 	    conn.commit()
 
 	if is_table_exist(database_name, 'Layout_Net') == None:
-	    cursor.execute('CREATE TABLE Layout_Net (uid text, start_shape text, net_shape text, remark text)')
+	    cursor.execute('CREATE TABLE Layout_Net (uid integer AUTOINCREMENT, start_shape text, net_shape text, layout_name text, remark text)')
 	    conn.commit()
 
 if __name__ == '__main__':
